3503a772357d7e0f3a97de5ed1dc9e2d84cd923f
BuRny <gamingburny@gmail.com>
2020-09-07 19:30:06 +0200

    Commit solutions

--- python_src/2020_09_september_contest/day_06.py
+++ python_src/2020_09_september_contest/day_06.py
@@ -1,34 +1,89 @@
-from typing import List
-
-""" Not my solution """
+from typing import List, Tuple
+import numpy as np
 
 
 class Solution:
-    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
-        dim = len(A)
+    @classmethod
+    def get_start_end(cls, array_length: int, shift: int) -> Tuple[int, int]:
+        start = shift if shift > 0 else 0
+        end = array_length if shift >= 0 else array_length + shift
+        return start, end
 
-        def shift_and_count(x_shift, y_shift, M, R):
-            """ 
-                Shift the matrix M, and count the ones in the overlapping zone.
-                M: matrix to be moved
-                R: matrix for reference
+    # @classmethod
+    # def get_start_end2(cls, array_length: int, shift: int) -> Tuple[int, int]:
+    #     start = 0 if shift >= 0 else -shift
+    #     end = array_length - shift if shift > 0 else array_length
+    #     return start, end
 
-                moving one matrix up and left is equivalent to
-                moving the other matrix down and right
-            """
-            count = 0
-            for r_row, m_row in enumerate(range(y_shift, dim)):
-                for r_col, m_col in enumerate(range(x_shift, dim)):
-                    if M[m_row][m_col] == 1 and M[m_row][m_col] == R[r_row][r_col]:
-                        count += 1
-            return count
+    def shift_and_count(self, a: np.ndarray, b: np.ndarray, x_shift: int, y_shift: int, size: int):
+        a_start_y, a_end_y = self.get_start_end(size, y_shift)
+        a_start_x, a_end_x = self.get_start_end(size, x_shift)
+        b_start_y = size - a_end_y
+        b_end_y = size - a_start_y
+        b_start_x = size - a_end_x
+        b_end_x = size - a_start_x
+        # print(size, a_start_y, a_end_y, b_start_y, b_end_y)
+        # b_start_y, b_end_y = self.get_start_end2(size, y_shift)
+        # b_start_x, b_end_x = self.get_start_end2(size, x_shift)
+        a_shifted = a[a_start_y:a_end_y, a_start_x:a_end_x]
+        b_shifted = b[b_start_y:b_end_y, b_start_x:b_end_x]
+        assert a_shifted.shape == b_shifted.shape, f"{a_shifted.shape} == {b_shifted.shape}"
+        return np.count_nonzero((a_shifted & b_shifted) == 1)
 
-        max_overlaps = 0
-        # move one of the matrice up and left and vice versa.
-        # (equivalent to move the other matrix down and right)
-        for y_shift in range(0, dim):
-            for x_shift in range(0, dim):
-                max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, A, B))
-                max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, B, A))
+    def largestOverlap(self, a: List[List[int]], b: List[List[int]]) -> int:
+        size = len(a)
+        count = 0
+        a_array = np.array(a)
+        b_array = np.array(b)
+        for x in range(-size + 1, size):
+            for y in range(-size + 1, size):
+                result = self.shift_and_count(a_array, b_array, x, y, size)
+                count = max(count, result)
+        return count
 
-        return max_overlaps
+
+if __name__ == "__main__":
+    # fmt: off
+    a = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ])
+    # fmt: on
+    length = len(a)
+    for y in range(-length + 1, length):
+        for x in range(-length + 1, length):
+            s1_y, e1_y = Solution.get_start_end(length, y)
+            s1_x, e1_x = Solution.get_start_end(length, x)
+            s2_y, e2_y = length - e1_y, length - s1_y
+            s2_x, e2_x = length - e1_x, length - s1_x
+            # s2_y, e2_y = Solution.get_start_end2(length, y)
+            # s2_x, e2_x = Solution.get_start_end2(length, x)
+            b: np.ndarray = a[s1_y:e1_y, s1_x:e1_x]
+            c: np.ndarray = a[s2_y:e2_y, s2_x:e2_x]
+            assert b.shape == c.shape, f"{(y, x)} - {b.shape} - {c.shape}"
+
+    # fmt: off
+    test_cases = [
+        [
+            [[1,1,0],
+            [0,1,0],
+            [0,1,0]],
+
+            [[0,0,0],
+            [0,1,1],
+            [0,0,1]]
+        ],
+    ]
+    results = [
+        3
+    ]
+    # fmt: on
+
+    app = Solution()
+    for test_case, correct_result in zip(test_cases, results):
+        test_case_copy = test_case.copy() if hasattr(test_case, "copy") else test_case
+        my_result = app.largestOverlap(*test_case)
+        assert (
+            my_result == correct_result
+        ), f"My result: {my_result}, correct result: {correct_result}\nTest Case: {test_case_copy}"
---
+++ python_src/2020_09_september_contest/day_07.py
@@ -0,0 +1,52 @@
+"""
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+https://leetcode.com/problems/pascals-triangle/
+"""
+from typing import List
+
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
+        string_split = str.split(" ")
+        if len(pattern) != len(string_split):
+            return False
+
+        set_of_pattern = set(pattern)
+        set_of_string = set(string_split)
+        if len(set_of_pattern) != len(set_of_string):
+            return False
+
+        assigned_chars = {}
+        already_assigned_words = set()
+        for char, word in zip(pattern, str.split(" ")):
+            if word not in already_assigned_words:
+                assigned_chars[char] = word
+                already_assigned_words.add(word)
+            elif assigned_chars.get(char, "") != word:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    # fmt: off
+    test_cases = [
+        ["abba", "dog cat cat dog"],
+        ["abba", "dog cat cat fish"],
+        ["aaaa", "dog cat cat dog"],
+        ["abba", "dog dog dog dog"],
+    ]
+    results = [
+        True,
+        False,
+        False,
+        False,
+    ]
+    # fmt: on
+
+    app = Solution()
+    for test_case, correct_result in zip(test_cases, results):
+        test_case_copy = test_case.copy() if hasattr(test_case, "copy") else test_case
+        my_result = app.wordPattern(*test_case)
+        assert (
+            my_result == correct_result
+        ), f"My result: {my_result}, correct result: {correct_result}\nTest Case: {test_case_copy}"
