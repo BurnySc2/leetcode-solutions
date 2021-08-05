a2046ce0da498daf4879ba454be327f67d44e624
BurnySc2 <gamingburny@gmail.com>
2021-08-05 23:02:12 +0200

    Add faster way to compute

--- python_src/project_euler/p760.py
+++ python_src/project_euler/p760.py
@@ -9,9 +9,9 @@ class Formula:
 
     def __str__(self):
         return f"g({self.m}, {self.n}) = {self.m} ^ {self.n} + {self.m} | {self.n} + {self.m} & {self.n} == {self.compute()}"
-        # return f"g({self.m}, {self.n}) = {self.m} ^ {self.n} == {self.compute()}"
-        # return f"g({self.m}, {self.n}) = {self.m} | {self.n} == {self.compute()}"
-        # return f"g({self.m}, {self.n}) = {self.m} & {self.n} == {self.compute()}"
+        # return f"g({self.m}, {self.n}) = {self.m} ^ {self.n} == {self.compute_slow()}"
+        # return f"g({self.m}, {self.n}) = {self.m} | {self.n} == {self.compute_slow()}"
+        # return f"g({self.m}, {self.n}) = {self.m} & {self.n} == {self.compute_slow()}"
 
     def compute(self) -> int:
         # return (self.m ^ self.n) + (self.m | self.n) + (self.m & self.n)
@@ -31,15 +31,213 @@ g(2, n-2) =
 
 """
 
-# def compute(m: int, n: int) -> int:
+# def compute_slow(m: int, n: int) -> int:
 #     return m ^ n + m | n + m & n
 
 
-def compute(N: int) -> int:
+def sum_formula(n: int) -> int:
+    return n * (n + 1) // 2
+
+
+"""
+"""
+
+
+def sum_formula_general(a: int, d: float, n: int) -> int:
+    """
+    SUM k=0 to n-1 of (a + k*d)
+    = n/2 (2a + (n-1)d)
+    https://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html
+    a = first term
+    d = difference between terms
+    n = amount of terms
+    """
+    assert n > 0, n
+    return n * (2 * a + int((n - 1) * d)) // 2
+
+
+def sum_formula_general_from_zero(d: int, n: int) -> int:
+    """
+    SUM k=0 to n-1 of (k*d)
+    = n/2 ((n-1)d)
+    https://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html
+    d = difference between terms
+    n = amount of terms
+    """
+    assert n > 0, n
+    return n * (n - 1) * d // 2
+
+
+def calc_avg(start: float, end: float) -> float:
+    return (start + end) / 2
+
+
+def compute_faster(N: int) -> int:
     debug_sums = {}
     my_sums = {}
     half_power_of_two = 1
     power_of_two = 1
+
+    for _ in range(int(math.log(N, 2)) + 1):
+        power_of_two *= 2
+        start_value = 2
+        terms_amount, max_remainder = divmod(N, power_of_two)
+        """
+        TODO instead of looping over remainder:
+        define min and max remainder, then put it into 3 categories
+        cat1 < cat2 < half_power_of_two <= cat3
+        cat1 < half_power_of_two <= cat2 < cat3
+        or sometimes it suffices:
+        cat1 < half_power_of_two <= cat2
+        
+        then calculate the averages on those instead
+        
+        cat1 = 0 
+        cat2 = max_remainder 
+        cat3 = power_of_two - 1
+        """
+
+        result = 0
+        result12 = 0
+        result34 = 0
+        top_correction = 0
+        bottom_correction = 0
+        if half_power_of_two > 0 and terms_amount >= 0:
+            # TOP CALCULATION - remainder 0 to half_power_of_two
+            top1_increment = power_of_two - 1
+            top1 = calc_avg(top1_increment, top1_increment * (terms_amount - 1))
+            top2_increment = half_power_of_two
+            top2 = calc_avg(top2_increment, top2_increment * (terms_amount - 1))
+
+            top12 = calc_avg(top1, top2)
+            amount_12_terms = half_power_of_two
+            result12 = top12 * amount_12_terms * (terms_amount - 1)
+
+            if 0 <= max_remainder < power_of_two:
+                top_correction_start = top1_increment * terms_amount
+                increment = -terms_amount
+                top12_correction_terms = half_power_of_two
+                if 0 <= max_remainder < half_power_of_two:
+                    top12_correction_terms = max_remainder + 1
+                top_correction = sum_formula_general(top_correction_start, increment, top12_correction_terms)
+
+
+            # BOTTOM CALCULATION - remainder half_power_of_two to power_of_two
+            bottom1_increment = half_power_of_two + 1
+            bottom1 = calc_avg(2, 2 + bottom1_increment * (terms_amount - 1))
+            bottom2_increment = power_of_two
+            bottom2 = calc_avg(bottom2_increment, bottom2_increment * terms_amount)
+
+            bottom12 = calc_avg(bottom1, bottom2)
+            result34 = bottom12 * amount_12_terms * terms_amount
+
+            if half_power_of_two == 32:
+                print()
+
+            if half_power_of_two <= max_remainder < power_of_two:
+                bottom_correction_start = 2 + bottom1_increment * terms_amount
+                increment = 2*(terms_amount+1)
+                boottom12_correction_terms = (max_remainder % half_power_of_two) + 1
+                bottom_correction = sum_formula_general(bottom_correction_start, increment, boottom12_correction_terms)
+
+            result = result12 + result34 + top_correction + bottom_correction
+
+        for remainder in range(power_of_two):
+            this_remainder_terms_amount = terms_amount + int(max_remainder >= remainder)
+            if this_remainder_terms_amount == 0 or this_remainder_terms_amount == 1 and remainder < half_power_of_two:
+                continue
+            debug_sums[half_power_of_two] = debug_sums.get(half_power_of_two, {})
+            debug_sums[half_power_of_two][remainder] = debug_sums[half_power_of_two].get(remainder, [])
+            if remainder >= half_power_of_two:
+                if this_remainder_terms_amount == 1:
+                    value = start_value
+                    debug_sums[half_power_of_two][remainder].append(f"{value} * 1")
+                    debug_sums[half_power_of_two][remainder].append(f"some filler")
+                else:
+                    difference_between_terms = abs(half_power_of_two - remainder - 1) + half_power_of_two
+                    debug_sums[half_power_of_two][remainder].append(
+                        f"E {start_value} {difference_between_terms} {this_remainder_terms_amount}"
+                    )
+                    end_value = start_value + ((this_remainder_terms_amount - 1) * difference_between_terms)
+                    avg = (start_value + end_value) / 2
+                    value = int(avg * this_remainder_terms_amount)
+                    debug_sums[half_power_of_two][remainder].append(f"{avg} * {this_remainder_terms_amount}")
+            else:
+                difference_between_terms = abs(half_power_of_two - remainder - 1) + half_power_of_two
+                debug_sums[half_power_of_two][remainder].append(
+                    f"E 0 {difference_between_terms} {this_remainder_terms_amount}"
+                )
+                if this_remainder_terms_amount == 2:
+                    value = difference_between_terms
+                    debug_sums[half_power_of_two][remainder].append(f"{value / 2} * 2")
+                else:
+                    end_value = (this_remainder_terms_amount - 1) * difference_between_terms
+                    avg = (difference_between_terms + end_value) / 2
+                    value = int(avg * (this_remainder_terms_amount - 1))
+                    debug_sums[half_power_of_two][remainder].append(f"{avg} * {this_remainder_terms_amount - 1}")
+
+            debug_sums[half_power_of_two][remainder].append(value)
+            my_sums[half_power_of_two] = my_sums.get(half_power_of_two, 0) + value
+            if remainder >= half_power_of_two:
+                start_value += 2
+        debug_sums[half_power_of_two]["topsum"] = sum(
+            debug_sums[half_power_of_two].get(r, [0, 0, 0])[2] for r in range(half_power_of_two)
+        )
+        debug_sums[half_power_of_two]["bottomsum"] = sum(
+            debug_sums[half_power_of_two].get(r, [0, 0, 0])[2] for r in range(half_power_of_two, power_of_two)
+        )
+        debug_sums[half_power_of_two]["slowsum"] = sum(
+            debug_sums[half_power_of_two].get(r, [0, 0, 0])[2] for r in range(power_of_two)
+        )
+        debug_sums[half_power_of_two]["quicktop"] = result12 + top_correction
+        debug_sums[half_power_of_two]["quickbottom"] = result34 + bottom_correction
+        debug_sums[half_power_of_two]["quicksum"] = result
+
+        half_power_of_two = power_of_two
+    corrected = {factor: factor * value for factor, value in my_sums.items() if isinstance(factor, int)}
+    result = 2 * sum(corrected.values())
+    return result
+
+
+# def compute_faster(N: int) -> int:
+#     debug_sums = {}
+#     my_sums = {}
+#     half_power_of_two = 1
+#     power_of_two = 1
+#
+#     for _ in range(int(math.log(N, 2)) + 1):
+#         power_of_two *= 2
+#         start_value = 2
+#         terms_amount, max_remainder = divmod(N, power_of_two)
+#         for remainder in range(power_of_two):
+#             difference_between_terms = abs(half_power_of_two - remainder - 1) + half_power_of_two
+#             this_remainder_terms_amount = terms_amount + int(max_remainder >= remainder)
+#             if this_remainder_terms_amount == 0 or this_remainder_terms_amount == 1 and remainder < half_power_of_two:
+#                 continue
+#             debug_sums[half_power_of_two] = debug_sums.get(half_power_of_two, {})
+#             debug_sums[half_power_of_two][remainder] = debug_sums[half_power_of_two].get(remainder, [])
+#             if remainder >= half_power_of_two:
+#                 value = sum_formula_general(start_value, difference_between_terms, this_remainder_terms_amount)
+#                 debug_sums[half_power_of_two][remainder].append(f"E {start_value} {difference_between_terms} {this_remainder_terms_amount}")
+#             else:
+#                 value = sum_formula_general_from_zero(difference_between_terms, this_remainder_terms_amount)
+#                 debug_sums[half_power_of_two][remainder].append(f"E 0 {difference_between_terms} {this_remainder_terms_amount}")
+#             debug_sums[half_power_of_two][remainder].append(value)
+#             my_sums[half_power_of_two] = my_sums.get(half_power_of_two, 0) + value
+#             if remainder >= half_power_of_two:
+#                 start_value += 2
+#         half_power_of_two = power_of_two
+#     corrected = {factor: factor * value for factor, value in my_sums.items() if isinstance(factor, int)}
+#     result = 2 * sum(corrected.values())
+#     return result
+
+
+def compute_slow(N: int) -> int:
+    debug_sums = {}
+    my_sums = {}
+    half_power_of_two = 1
+    power_of_two = 1
+
     for _ in range(int(math.log(N, 2)) + 1):
         power_of_two *= 2
         for number in range(half_power_of_two, N + 1):
@@ -50,13 +248,13 @@ def compute(N: int) -> int:
             if remainder >= half_power_of_two:
                 summand_after = 2 * (remainder - half_power_of_two + 1)
             value = factor_left * factor_right + summand_after
-            debug_sums[half_power_of_two] = debug_sums.get(half_power_of_two, [])
-            debug_sums[half_power_of_two].append(value)
+            debug_sums[half_power_of_two] = debug_sums.get(half_power_of_two, {})
+            debug_sums[half_power_of_two][remainder] = debug_sums[half_power_of_two].get(remainder, [])
+            debug_sums[half_power_of_two][remainder].append(value)
             my_sums[half_power_of_two] = my_sums.get(half_power_of_two, 0) + value
         half_power_of_two = power_of_two
-    corrected = {factor: factor * value for factor, value in my_sums.items()}
+    corrected = {factor: factor * value for factor, value in my_sums.items() if isinstance(factor, int)}
     result = 2 * sum(corrected.values())
-
     return result
 
 
@@ -75,37 +273,54 @@ def main():
     for row in my_sum:
         print(" ".join(map(str, row)))
 
-    max_value = max(i for row in my_sum for i in row)
-    i = 0
-    while 1:
-        print_triangle = []
-        two = 2**i
-        if max_value < two:
-            break
-        for row in my_sum:
-            print_row = []
-            for cell in row:
-                print_row.append(int(cell | two == cell))
-            print_triangle.append(print_row)
-        print(f"\nnumber | {two} == number")
-        for row in print_triangle:
-            row_copy = row.copy()
-            row_copy.insert(0, sum(row))
-            print(sum(row))
-            # print(" ".join(map(str, row_copy)))
-            # print(" ".join(map(str, row)))
-        i += 1
+    # max_value = max(i for row in my_sum for i in row)
+    # i = 0
+    # while 1:
+    #     print_triangle = []
+    #     two = 2**i
+    #     if max_value < two:
+    #         break
+    #     for row in my_sum:
+    #         print_row = []
+    #         for cell in row:
+    #             print_row.append(int(cell | two == cell))
+    #         print_triangle.append(print_row)
+    #     print(f"\nnumber | {two} == number")
+    #     for row in print_triangle:
+    #         row_copy = row.copy()
+    #         row_copy.insert(0, sum(row))
+    #         print(sum(row))
+    #         # print(" ".join(map(str, row_copy)))
+    #         # print(" ".join(map(str, row)))
+    #     i += 1
 
-    result_10 = compute(10)
+    result_10 = compute_slow(10)
+    # result2_10 = compute_faster(10)
     print(f"G(10) == {result_10}")
-    result_100 = compute(100)
+    # print(f"G(10) == {result2_10}")
+
+    result_100 = compute_slow(100)
+    result2_100 = compute_faster(100)
+
+    # import time
+    #
+    # t1 = time.perf_counter()
+    # _ = compute_faster(10**6)
+    # t2 = time.perf_counter()
+    # print(f"{t2 - t1}")
+
     print(f"G(100) == {result_100}")
-    result_1000 = compute(1000)
+    print(f"G(100) == {result2_100}")
+
+    result_1000 = compute_slow(1000)
+    result2_1000 = compute_faster(1000)
     print(f"G(1000) == {result_1000}")
+    print(f"G(1000) == {result2_1000}")
+
     assert result_10 == 754, f"{result_10} != 754"
     assert result_100 == 583_766, f"{result_100} != 583_766"
     assert result_1000 == 580_621_308, f"{result_1000} != 580_621_308"
-    # result_n = compute(10**7)
+    # result_n = compute_slow(10**7)
 
     # result = []
     # for i in my_sum:
